f9d7647350fcc6b0cb3006409d40dd354c118498
Anuj Kant <anujkant1@gmail.com>
2022-01-23 19:04:58 +0000

    Displays digital clock of the current time

---
+++ bigbook/DigitalClock.py
@@ -0,0 +1,50 @@
+"""Digital Clock
+Displays a digital clock of the current time with a seven-segment
+display. Press Ctrl-c to stop.
+More info at https://en.wikipedia.org/wiki/Seven-segment_display
+Requires sevseg.py to be in the same folder."""
+
+import sys
+import time
+import sevseg  # Imports sevseg.py program
+
+try:
+    while True:  # Main program loop
+        # Clear the screen by printing several newlines:
+        print('\n' * 80)
+
+        # Get the current time from the computer's clock:
+        currentTime = time.localtime()
+        # % 12 so we use a 12-hour clock, not 24:
+        hours = str(currentTime.tm_hour % 12)
+        if hours == '0':
+            hours = '12'  # 12-hour clock show 12:00, not 00:00
+        minutes = str(currentTime.tm_min)
+        seconds = str(currentTime.tm_sec)
+
+        # Get the digit string from the sevseg module:
+        hDigits = sevseg.getSevSegStr(hours, 2)
+        hTopRow, hMiddleRow, hBottomRow = hDigits.splitlines()
+
+        mDigits = sevseg.getSevSegStr(minutes, 2)
+        mTopRow, mMiddleRow, mBottomRow = mDigits.splitlines()
+
+        sDigits = sevseg.getSevSegStr(seconds, 2)
+        sTopRow, sMiddleRow, sBottomRow = sDigits.splitlines()
+
+        # Display the digits:
+        print(f'{hTopRow   }     {mTopRow   }     {sTopRow   }')
+        print(f'{hMiddleRow}  *  {mMiddleRow}  *  {sMiddleRow}')
+        print(f'{hBottomRow}  *  {mBottomRow}  *  {sBottomRow}')
+        print()
+        print('Press Ctrl-C to quit.')
+
+        # Keep looping until the second changes:
+        while True:
+            time.sleep(0.01)
+            if time.localtime().tm_sec != currentTime.tm_sec:
+                break
+
+except KeyboardInterrupt:
+    print('Digital Clock.')
+    sys.exit()  # When Ctrl-C is pressed, end the program.
