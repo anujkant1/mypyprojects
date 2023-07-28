fa21fc2991890f63e5c5642606d64fbe3ae9aaf9
anujkant1 <anujkant1@gmail.com>
2023-07-28 07:00:10 +0100

    Abacus code optimisation

--- bigbook/soroban.py
+++ bigbook/soroban.py
@@ -13,6 +13,16 @@ def main():
 
     abacusNumber = 0  # This is the number represented on the abacus.
 
+    add_dict = {
+        'q': 1000000000, 'w': 100000000, 'e': 10000000, 'r': 1000000,
+        't': 100000, 'y': 10000, 'u': 1000, 'i': 100, 'o': 10, 'p': 1
+    }
+
+    subtract_dict = {
+        'a': 1000000000, 's': 100000000, 'd': 10000000, 'f': 1000000,
+        'g': 100000, 'h': 10000, 'j': 1000, 'k': 100, 'l': 10, ';': 1
+    }
+
     while True:  # Main program loop.
         displayAbacus(abacusNumber)
         displayControls()
@@ -27,46 +37,10 @@ def main():
         else:
             # Handle increment/decrement commands:
             for letter in commands:
-                if letter == 'q':
-                    abacusNumber += 1000000000
-                elif letter == 'a':
-                    abacusNumber -= 1000000000
-                elif letter == 'w':
-                    abacusNumber += 100000000
-                elif letter == 's':
-                    abacusNumber -= 100000000
-                elif letter == 'e':
-                    abacusNumber += 10000000
-                elif letter == 'd':
-                    abacusNumber -= 10000000
-                elif letter == 'r':
-                    abacusNumber += 1000000
-                elif letter == 'f':
-                    abacusNumber -= 1000000
-                elif letter == 't':
-                    abacusNumber += 100000
-                elif letter == 'g':
-                    abacusNumber -= 100000
-                elif letter == 'y':
-                    abacusNumber += 10000
-                elif letter == 'h':
-                    abacusNumber -= 10000
-                elif letter == 'u':
-                    abacusNumber += 1000
-                elif letter == 'j':
-                    abacusNumber -= 1000
-                elif letter == 'i':
-                    abacusNumber += 100
-                elif letter == 'k':
-                    abacusNumber -= 100
-                elif letter == 'o':
-                    abacusNumber += 10
-                elif letter == 'l':
-                    abacusNumber -= 10
-                elif letter == 'p':
-                    abacusNumber += 1
-                elif letter == ';':
-                    abacusNumber -= 1
+                if letter in add_dict:
+                    abacusNumber += add_dict[letter]
+                elif letter in subtract_dict:
+                    abacusNumber -= subtract_dict[letter]
 
         # The abacus can't show negative numbers:
         if abacusNumber < 0:
