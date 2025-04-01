152583d1969671cdaada8d08a3f5777d934c910f
anujkant1 <anujkant1@gmail.com>
2025-04-01 17:46:55 +0100

    Optimising childrenadditiongame

--- LrngInPrgrs/childrenaddtiongame.py
+++ LrngInPrgrs/childrenaddtiongame.py
@@ -1,34 +1,40 @@
 """
 Children's Addition Game
-This game will ask children to add two numbers and then check if the answer is correct.
+This game will ask children to add two numbers and then check if it's correct.
 The twist is that it will ask if they are serious or want a funny answer."""
 
 import random
 import sys
 
+
 def get_random_numbers():
     return random.randint(1, 20), random.randint(1, 20)
+
 
 def get_child_response(num1, num2):
     print(f"What is {num1} + {num2}?")
     return input(">  ")
+
 
 def get_response_type():
     print("Have you provided the actual answer or THE FUNNY answer?")
     print("Serious(s) of Funny(f)")
     return input("> ")
 
+
 def check_serious_answer(num1, num2, child_response):
     if num1 + num2 == int(child_response):
         return "Your answer is actually correct!"
     else:
         return f"Sorry, the actually answer is {num1 + num2} and not {child_response}."
-    
+
+
 def check_funny_answer(num1, num2, child_response):
     if str(num1) + str(num2) == child_response:
         return "Your FUNNY answer is correct!"
     else:
-        return f"Sorry, funny answer I was expecting was {num1}{num2}, so your funny answer {child_response} is not correct!"
+        return f"Sorry, the expected funny answer was {num1}{num2} so your funny answer {child_response} is not correct!"
+
 
 def main():
     while True:
@@ -50,7 +56,8 @@ def main():
             if play_again.lower() != "y":
                 sys.exit()
         except ValueError:
-            print("You have not provided a valid number. Please provide a number between 1 and 9.")
+            print("You have not provided a valid number.")
+            print("Please provide a number between 1 and 9.")
             continue
         except KeyboardInterrupt:
             print("\nYou have pressed Ctrl+C. Exiting the game.")
